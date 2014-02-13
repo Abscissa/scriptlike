dfd7900ca26fe26eaea3ccf8a905a8cbe52829e8
Nick Sabalausky <Abscissa>
2014-02-13 05:01:51 -0500

    fail: No longer requires any boilerplate in main().

--- src/scriptlike/fail.d
+++ src/scriptlike/fail.d
@@ -3,39 +3,68 @@
 
 module scriptlike.fail;
 
-/++
-In your main(), catch this Fail exception, then output Fail.msg and
-return an error code.
+import std.file;
+import std.path;
+import std.traits;
 
-Example:
-
-int main()
-{
-	try
-	{
-		// Your code here
-	}
-	catch(Fail e)
-	{
-		writeln("mytool: ERROR: ", e.msg);
-		return 1;
-	}
-	
-	return 0;
-}
-+/
+/// This is the exception thrown by fail(). There's no need to create or throw
+/// this directly, but it's public in case you have reason to catch it.
 class Fail : Exception
 {
-	this(string msg, string file=__FILE__, int line=__LINE__)
+	private this()
 	{
-		super(msg, file, line);
+		super(null);
+	}
+
+	private static Fail opCall(string msg, string file=__FILE__, int line=__LINE__)
+	{
+		auto f = cast(Fail) cast(void*) Fail.classinfo.init;
+		
+		f.msg  = msg;
+		f.file = file;
+		f.line = line;
+		
+		throw f;
+	}
+	
+	// Throwable.toString(sink) isn't an override on DMD 2.064.2, and druntime
+	// won't even call this on DMD 2.064.2 anyway, so don't even bother
+	// including this function if Throwable doesn't have toString(sink).
+	static if( MemberFunctionsTuple!(Throwable, "toString").length > 1 )
+	{
+		override void toString(scope void delegate(in char[]) sink) const
+		{
+			auto appName = thisExePath().baseName();
+
+			version(Windows)
+				appName = appName.stripExtension();
+
+			sink(appName~": ERROR: "~msg);
+		}
 	}
 }
 
-/// If you've set up your main() to handle the Fail exception (as shown in
-/// Fail's documentation, then call this to end your program with an error
-/// message in an exception-safe way.
+/++
+Call this to end your program with an error message for the user, and no
+ugly stack trace. The error message is sent to stderr and the errorlevel is
+set to non-zero.
+
+This is exception-safe, all cleanup code gets run.
+
+Your program's name is automatically detected from std.file.thisExePath.
+
+Example:
+----------------
+fail("You forgot to provide a destination!");
+
+// Output on DMD 2.065 and up:
+// yourProgramName: ERROR: You forgot to provide a destination!
+
+// Output on DMD 2.064.2:
+// scriptlike.fail.Fail@yourFilename.d(71): You forgot to provide a destination!
+----------------
++/
 void fail(string msg, string file=__FILE__, int line=__LINE__)
 {
-	throw new Fail(msg, file, line);
+	throw Fail(msg, file, line);
 }
