1266ae96c3ff5689338e744315876949ca330c95
Nick Sabalausky <Abscissa@users.noreply.github.com>
2015-09-13 11:45:45 -0400

    More scriptlike.process tests.

--- src/scriptlike/file/extras.d
+++ src/scriptlike/file/extras.d
@@ -36,6 +36,36 @@ bool existsAsDir(in Path path) @trusted
 	return existsAsDir(path.toRawString());
 }
 
+version(unittest_scriptlike_d)
+unittest
+{
+	string file, dir, notExist;
+
+	testFileOperation!("existsAsDir", "string")(() {
+		mixin(useTmpName!"file");
+		mixin(useTmpName!"dir");
+		mixin(useTmpName!"notExist");
+		std.file.write(file, "abc123");
+		std.file.mkdir(dir);
+
+		assert( !existsAsDir(file) );
+		assert( existsAsDir(dir) );
+		assert( !existsAsDir(notExist) );
+	});
+
+	testFileOperation!("existsAsDir", "Path")(() {
+		mixin(useTmpName!"file");
+		mixin(useTmpName!"dir");
+		mixin(useTmpName!"notExist");
+		std.file.write(file, "abc123");
+		std.file.mkdir(dir);
+
+		assert( !existsAsDir(Path(file)) );
+		assert( existsAsDir(Path(dir)) );
+		assert( !existsAsDir(Path(notExist)) );
+	});
+}
+
 /// Checks if the path exists as a file.
 ///
 /// This is like $(FULL_STD_FILE isFile), but returns false instead of
@@ -51,6 +81,36 @@ bool existsAsFile(in Path path) @trusted
 	return existsAsFile(path.toRawString());
 }
 
+version(unittest_scriptlike_d)
+unittest
+{
+	string file, dir, notExist;
+
+	testFileOperation!("existsAsFile", "string")(() {
+		mixin(useTmpName!"file");
+		mixin(useTmpName!"dir");
+		mixin(useTmpName!"notExist");
+		std.file.write(file, "abc123");
+		std.file.mkdir(dir);
+
+		assert( existsAsFile(file) );
+		assert( !existsAsFile(dir) );
+		assert( !existsAsFile(notExist) );
+	});
+
+	testFileOperation!("existsAsFile", "Path")(() {
+		mixin(useTmpName!"file");
+		mixin(useTmpName!"dir");
+		mixin(useTmpName!"notExist");
+		std.file.write(file, "abc123");
+		std.file.mkdir(dir);
+
+		assert( existsAsFile(Path(file)) );
+		assert( !existsAsFile(Path(dir)) );
+		assert( !existsAsFile(Path(notExist)) );
+	});
+}
+
 /// Checks if the path exists as a symlink.
 ///
 /// This is like $(FULL_STD_FILE isSymlink), but returns false instead of
@@ -64,6 +124,60 @@ bool existsAsSymlink()(in string path) @trusted
 bool existsAsSymlink(in Path path) @trusted
 {
 	return existsAsSymlink(path.toRawString());
+}
+
+version(unittest_scriptlike_d)
+unittest
+{
+	string file, dir, fileLink, dirLink, notExist;
+
+	testFileOperation!("existsAsSymlink", "string")(() {
+		mixin(useTmpName!"file");
+		mixin(useTmpName!"dir");
+		mixin(useTmpName!"fileLink");
+		mixin(useTmpName!"dirLink");
+		mixin(useTmpName!"notExist");
+		std.file.write(file, "abc123");
+		std.file.mkdir(dir);
+		version(Posix)
+		{
+			std.file.symlink(file, fileLink);
+			std.file.symlink(dir, dirLink);
+		}
+
+		assert( !existsAsSymlink(file) );
+		assert( !existsAsSymlink(dir) );
+		assert( !existsAsSymlink(notExist) );
+		version(Posix)
+		{
+			assert( existsAsSymlink(fileLink) );
+			assert( existsAsSymlink(dirLink) );
+		}
+	});
+
+	testFileOperation!("existsAsSymlink", "Path")(() {
+		mixin(useTmpName!"file");
+		mixin(useTmpName!"dir");
+		mixin(useTmpName!"fileLink");
+		mixin(useTmpName!"dirLink");
+		mixin(useTmpName!"notExist");
+		std.file.write(file, "abc123");
+		std.file.mkdir(dir);
+		version(Posix)
+		{
+			std.file.symlink(file, fileLink);
+			std.file.symlink(dir, dirLink);
+		}
+
+		assert( !existsAsSymlink(Path(file)) );
+		assert( !existsAsSymlink(Path(dir)) );
+		assert( !existsAsSymlink(Path(notExist)) );
+		version(Posix)
+		{
+			assert( existsAsSymlink(Path(fileLink)) );
+			assert( existsAsSymlink(Path(dirLink)) );
+		}
+	});
 }
 
 /// If 'from' exists, then rename. Otherwise do nothing.
@@ -83,6 +197,85 @@ bool tryRename(T1, T2)(T1 from, T2 to)
 	}
 
 	return false;
+}
+
+version(unittest_scriptlike_d)
+unittest
+{
+	string file1, file2, notExist1, notExist2;
+	void checkPre()
+	{
+		assert(!std.file.exists(notExist1));
+		assert(!std.file.exists(notExist2));
+
+		assert(!std.file.exists(file2));
+		assert(std.file.exists(file1));
+		assert(std.file.isFile(file1));
+		assert(cast(string) std.file.read(file1) == "abc");
+	}
+
+	void checkPost()
+	{
+		assert(!std.file.exists(notExist1));
+		assert(!std.file.exists(notExist2));
+
+		assert(!std.file.exists(file1));
+		assert(std.file.exists(file2));
+		assert(std.file.isFile(file2));
+		assert(cast(string) std.file.read(file2) == "abc");
+	}
+
+	testFileOperation!("tryRename", "string,string")(() {
+		mixin(useTmpName!"file1");
+		mixin(useTmpName!"file2");
+		mixin(useTmpName!"notExist1");
+		mixin(useTmpName!"notExist2");
+		std.file.write(file1, "abc");
+
+		checkPre();
+		tryRename(file1, file2);
+		tryRename(notExist1, notExist2);
+		mixin(checkResult);
+	});
+
+	testFileOperation!("tryRename", "string,Path")(() {
+		mixin(useTmpName!"file1");
+		mixin(useTmpName!"file2");
+		mixin(useTmpName!"notExist1");
+		mixin(useTmpName!"notExist2");
+		std.file.write(file1, "abc");
+
+		checkPre();
+		tryRename(file1, Path(file2));
+		tryRename(notExist1, Path(notExist2));
+		mixin(checkResult);
+	});
+
+	testFileOperation!("tryRename", "Path,string")(() {
+		mixin(useTmpName!"file1");
+		mixin(useTmpName!"file2");
+		mixin(useTmpName!"notExist1");
+		mixin(useTmpName!"notExist2");
+		std.file.write(file1, "abc");
+
+		checkPre();
+		tryRename(Path(file1), file2);
+		tryRename(Path(notExist1), notExist2);
+		mixin(checkResult);
+	});
+
+	testFileOperation!("tryRename", "Path,Path")(() {
+		mixin(useTmpName!"file1");
+		mixin(useTmpName!"file2");
+		mixin(useTmpName!"notExist1");
+		mixin(useTmpName!"notExist2");
+		std.file.write(file1, "abc");
+
+		checkPre();
+		tryRename(Path(file1), Path(file2));
+		tryRename(Path(notExist1), Path(notExist2));
+		mixin(checkResult);
+	});
 }
 
 /// If 'name' exists, then remove. Otherwise do nothing.
