1e8e36fbcf3ccfbeca63aa2e6bbc522abcbdc4af
Nick Sabalausky <Abscissa@users.noreply.github.com>
2017-01-31 00:52:25 -0500

    fail: Remove fallback stuff for older DMDs that aren't even supported by Scriptlike anymore.

--- src/scriptlike/fail.d
+++ src/scriptlike/fail.d
@@ -12,14 +12,6 @@ import std.file;
 import std.path;
 import std.traits;
 
-// Throwable.toString(sink) isn't an override on DMD 2.064.2, and druntime
-// won't even call any Throwable.toString on DMD 2.064.2 anyway, so use
-// a fallback method if Throwable doesn't have toString(sink).
-static if( MemberFunctionsTuple!(Throwable, "toString").length > 1 )
-	private enum useFallback = false;
-else
-	private enum useFallback = true;
-
 /// This is the exception thrown by fail(). There's no need to create or throw
 /// this directly, but it's public in case you have reason to catch it.
 class Fail : Exception
@@ -51,12 +43,9 @@ class Fail : Exception
 		return appName~": ERROR: "~msg;
 	}
 	
-	static if(!useFallback)
+	override void toString(scope void delegate(in char[]) sink) const
 	{
-		override void toString(scope void delegate(in char[]) sink) const
-		{
-			sink(fullMessage());
-		}
+		sink(fullMessage());
 	}
 }
 
@@ -69,33 +58,17 @@ This is exception-safe, all cleanup code gets run.
 
 Your program's name is automatically detected from $(STD_FILE thisExePath).
 
-Note, on DMD 2.064.2, the error message is displayed BEFORE the exception is
-thrown. So if you catch the Fail exception, the message will have already been
-displayed. This is due to limitations in the older druntime, and is fixed
-on DMD 2.065 and up.
-
 Example:
 ----------------
 auto id = 3;
 fail("You forgot to provide a destination for id #", id, "!");
 
-// Output on DMD 2.065 and up:
+// Output:
 // yourProgramName: ERROR: You forgot to provide a destination for id #3!
-
-// Output on DMD 2.064.2:
-// yourProgramName: ERROR: You forgot to provide a destination for id #3!
-// scriptlike.fail.Fail
 ----------------
 +/
 void fail(T...)(T args)
 {
-	static if(useFallback)
-	{
-		import std.stdio;
-		stderr.writeln(Fail.fullMessage( text(args) ));
-		stderr.flush();
-	}
-	
 	throw Fail( text(args) );
 }
 
@@ -109,12 +82,8 @@ Example:
 ----------------
 failEnforce(brokenSquareRoot(4)==2, "Reality broke! Expected 2, not ", brokenSquareRoot(4));
 
-// Output on DMD 2.065 and up:
+// Output:
 // yourProgramName: ERROR: Reality broke! Expected 2, not 555
-
-// Output on DMD 2.064.2:
-// yourProgramName: ERROR: Reality broke! Expected 2, not 555
-// scriptlike.fail.Fail
 ----------------
 +/
 void failEnforce(T...)(bool cond, T args)
